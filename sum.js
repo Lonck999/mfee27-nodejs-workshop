b6ff2dd31e2a4b6246a9112424638896d980e3b3
Lonck999 <108327193+Lonck999@users.noreply.github.com>
2022-07-30 12:33:21 +0800

    add new sum
    add new sum

--- sum.js
+++ sum.js
@@ -1,17 +1,21 @@
-function sum(n) {
-    let result = 0;
-    for (let i = 1; i <= n; i++) {
-      result += i;
+// function sum(n) {
+//     let result = 0;
+//     for (let i = 1; i <= n; i++) {
+//       result += i;
+//     }
+//     return result;
+//   }
+
+
+//這是新的
+  function sum(n) {
+    if (n === 1) {
+      return 1;
     }
-    return result;
+    return n + sum(n - 1);
   }
 
-//   function sum(n) {
-//     if (n === 1) {
-//       return 1;
-//     }
-//     return n + sum(n - 1);
-//   }
+  
   console.log(sum(1));
   console.log(sum(3));
   console.log(sum(10));
