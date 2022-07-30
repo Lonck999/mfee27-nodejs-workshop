a28dc3a1771a02b7157a1a278e103286541fc384
Lonck999 <108327193+Lonck999@users.noreply.github.com>
2022-07-30 12:31:22 +0800

    add new sum
    add new sum

--- sum.js
+++ sum.js
@@ -6,6 +6,12 @@ function sum(n) {
     return result;
   }
 
+//   function sum(n) {
+//     if (n === 1) {
+//       return 1;
+//     }
+//     return n + sum(n - 1);
+//   }
   console.log(sum(1));
   console.log(sum(3));
   console.log(sum(10));
