2d3c25d8f9ae5fe62323320e427262d63dc4084f
Lonck999 <108327193+Lonck999@users.noreply.github.com>
2022-08-14 12:38:30 +0800

    add  crawler3.js

---
+++ crawler/crawler3.js
@@ -0,0 +1,28 @@
+// 1. 自動取得今日日期 （可能利用 cron 排程工具 系統每日自動執行）
+// 2. 從檔案中讀取股票代碼
+const axios = require('axios');
+const moment = require('moment');
+const fs = require('fs');  
+const fsPromises = fs.promises;
+
+
+// 開始抓資料
+// 2603 嘉里大榮
+// let stockNo = 2603; // TODO: 需要從 .txt 的檔案裡讀取股票代碼
+let queryDate = moment().format('YYYYMMDD'); //'20220814';
+// axios.get(url, 設定)
+(async () => {
+  try {
+    const stockNo = await fsPromises.readFile('stock.txt', 'utf-8'); 
+    let response = await axios.get(`https://www.twse.com.tw/exchangeReport/STOCK_DAY`, {
+      params: {
+        response: 'json',
+        date: queryDate,
+        stockNo: stockNo,
+      },
+    });
+    console.log(response.data);
+  } catch (e) {
+    console.error(e);
+  }
+})();
